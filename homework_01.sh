1bbe033f658a8aae2501793b93ec1516efbd61f0
jeremymuhia <jmuhia80@gmail.com>
2018-02-04 15:16:43 -0500

    test without input

--- homework_01.sh
+++ homework_01.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
+echo "### Testing mad_libs_lyrics.py\n"
+python3 mad_libs_lyrics.py
+echo ""
+
+echo "### Testing exercises.py\n"
+python3 exercises.py
+echo ""
+
+echo "### Testing tree.py\n"
 python3 tree.py
+echo ""
