e7dbe578926865a7ab8562162fcff3ccdaf85d97
jeremymuhia <jmuhia80@gmail.com>
2018-02-24 19:25:08 -0500

    done testing grade.py

--- homework_03.sh
+++ homework_03.sh
@@ -1,1 +1,20 @@
 #!/bin/sh
+
+echo "### Testing grade.py\n"
+grade_out="$((python3 grade.py <<EOF
+-12
+EOF
+) 2>&1)
+$((python3 grade.py <<EOF
+69
+EOF
+) 2>&1)"
+
+if [[ "${grade_out}" == *"translate"* ]] && \
+    [[ "${grade_out}" == *"D"* ]]; then
+        echo "VVV All grade.py tests pass\n"
+    else
+        echo "XXX Some grade.py tests do not pass\n"
+    fi
+
+echo "${grade_out}\n"
