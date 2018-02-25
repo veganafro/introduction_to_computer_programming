b4f1837fc14c8251a58799cd8e0037f1c3284224
jeremymuhia <jmuhia80@gmail.com>
2018-02-25 14:40:54 -0500

    writing second tests

--- homework_03.sh
+++ homework_03.sh
@@ -22,10 +22,17 @@ echo "${grade_out}\n"
 
 echo "### Testing tip.py\n"
 tip_out="$((python3 tip.py <<EOF
-
+6
+10
 EOF
 ) 2>&1)
 $((python3 tip.py <<EOF
-
+3
+20
+good
 EOF
 ) 2>&1)"
+
+if [[ "${tip_out}" == *'$2'* ]] && \
+    [[ "${tip_out}" == *'$4'* ]]; then
+        echo "VVV All"
