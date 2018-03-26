487b2a9b62a1565599131c94cbdbcf6f18b95c0a
jeremymuhia <jmuhia80@gmail.com>
2018-03-25 21:57:05 -0400

    build homework 4 frame

---
+++ homework_04.sh
@@ -0,0 +1,33 @@
+echo "### Testing many_words.py"
+
+
+
+echo "### Done testing many_workds.py"
+
+
+echo "### Testing odd_and_evens.py"
+
+
+
+echo "### Done testing odd_and_evens.py"
+
+
+echo "### Testing flagged.py"
+
+
+
+echo "### Done testing flagged.py"
+
+
+echo "### Testing average_word.py"
+
+
+
+echo "### Done testing average_word.py"
+
+
+echo "### Testing counting.py"
+
+
+
+echo "### Done testing counting.py"
