7e28ab47a1aa513bac824bdf91e0d69cfb15fe04
jeremymuhia <jmuhia80@gmail.com>
2018-03-25 22:40:43 -0400

    working on many words

--- homework_04.sh
+++ homework_04.sh
@@ -1,8 +1,22 @@
-echo "### Testing many_words.py"
+echo "### Testing many_words.py\n"
+
+many_words_out=$((
+		    python3 many_words.py <<EOF
+		    4
+		    program
+		    my
+		    out
+		    check
+		    EOF
+		) 2>&1)
+
+if [[ $many_words_out == *"check out my program"*]]; then
+    echo "VVV All many_words.py tests pass\n"
+else
+    echo "XXX Some many_words.py tests do not pass\n"
 
 
-
-echo "### Done testing many_workds.py"
+echo "### Done testing many_workds.py\n"
 
 
 echo "### Testing odd_and_evens.py"
