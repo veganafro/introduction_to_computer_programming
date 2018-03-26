d4a5424a07305b1f1a071e152241353d7aaff451
jeremymuhia <jmuhia80@gmail.com>
2018-03-25 23:58:52 -0400

    finish odds and evens

--- homework_04.sh
+++ homework_04.sh
@@ -1,47 +1,74 @@
+#!/bin/sh
+
 echo "### Testing many_words.py\n"
 
-many_words_out=$((
-		    python3 many_words.py <<EOF
-		    4
-		    program
-		    my
-		    out
-		    check
-		    EOF
-		) 2>&1)
+many_words_out=$(
+    (
+	python3 many_words.py <<-EOF
+	4
+	program
+	my
+	out
+	check
+	EOF
+    ) 2>&1)
 
-if [[ $many_words_out == *"check out my program"*]]; then
+if [[ "${many_words_out}" == *"check out my program"* ]]; then
     echo "VVV All many_words.py tests pass\n"
 else
     echo "XXX Some many_words.py tests do not pass\n"
+fi
+
+echo "${many_words_out}\n### Done testing many_workds.py\n"
 
 
-echo "### Done testing many_workds.py\n"
+echo "### Testing odds_and_evens.py\n"
+
+odds_and_evens_out_1=$(
+    (
+	python3 odds_and_evens.py <<-EOF
+	Derp
+	odds
+	0
+	EOF
+    ) 2>&1)
+
+odds_and_evens_out_2=$(
+    (
+	python odds_and_evens.py <<-EOF
+	evens
+	2
+	1
+	1
+	EOF
+    ) 2>&1)
+
+if [[ "${odds_and_evens_out_1}" == *"NO ONE"* ]] && \
+       [[ "${odds_and_evens_out_2}" == *"wins"* ]]; then
+    echo "VVV All odds_and_evens.py tests pass\n"
+else
+    echo "XXX Some odds_and_evens.py tests do not pass\n"
+fi
+
+echo "${odds_and_evens_out_1}\n${odds_and_evens_out_2}\n### Done testing odd_and_evens.py"
 
 
-echo "### Testing odd_and_evens.py"
+echo "### Testing flagged.py\n"
 
 
 
-echo "### Done testing odd_and_evens.py"
+echo "### Done testing flagged.py\n"
 
 
-echo "### Testing flagged.py"
-
-
-
-echo "### Done testing flagged.py"
-
-
-echo "### Testing average_word.py"
+echo "### Testing average_word.py\n"
 
 
 
-echo "### Done testing average_word.py"
+echo "### Done testing average_word.py\n"
 
 
-echo "### Testing counting.py"
+echo "### Testing counting.py\n"
 
 
 
-echo "### Done testing counting.py"
+echo "### Done testing counting.py\n"
