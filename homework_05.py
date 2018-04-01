ae7b8d11b762d987ed2cbccff47e37b58e05cf2c
jeremymuhia <jmuhia80@gmail.com>
2018-04-01 10:41:24 -0400

    switching to unittest module

--- homework_05.py
+++ homework_05.py
@@ -2,6 +2,9 @@ import unittest
 import analyze_numbers
 
 class Homework_05(unittest.TestCase):
-    
+
+    def test_analyze_true(self):
+        self.assertTrue(analyze_numbers.is_odd(69))
+        
 if __name__ = '__main__':
     unittest.main()
