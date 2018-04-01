e7c6009746c6c05a1da4f080b222ed821ec7471f
jeremymuhia <jmuhia80@gmail.com>
2018-04-01 13:20:56 -0400

    building tests

--- homework_05.py
+++ homework_05.py
@@ -5,6 +5,9 @@ class Homework_05(unittest.TestCase):
 
     def test_analyze_true(self):
         self.assertTrue(analyze_numbers.is_odd(69))
+
+    def test_analyze_false(self):
+        self.assertFalse(analyze_numbers.is_odd(66))
         
 if __name__ = '__main__':
     unittest.main()
