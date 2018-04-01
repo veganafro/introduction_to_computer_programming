2731456344f900e25ab95cad1ee803a82aea3441
jeremymuhia <jmuhia80@gmail.com>
2018-04-01 18:41:15 -0400

    building more tests

--- bash_tests/homework_05.sh
+++ bash_tests/homework_05.sh
@@ -5,6 +5,13 @@ echo "### Testing slashes.py\n"
 slashes_out=$(
     (
 	python3 slashes.py <<-EOF
-	
+	3
+	2
 	EOF
-    ))
+    ) 2>&1)
+
+if [[ "${slashes_out}" == *"|/|\n|\\|\n|/|"* ]]; then
+    echo "VVV All slashes.py tests pass\n"
+else
+    echo "XXX Some slashes.py tests do not pass\n"
+fi
