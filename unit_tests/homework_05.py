2ac89f2b1d675d294fa9425b1a2202159b30251b
jeremymuhia <jmuhia80@gmail.com>
2018-04-01 20:02:18 -0400

    writing more tests

--- unit_tests/homework_05.py
+++ unit_tests/homework_05.py
@@ -1,40 +1,62 @@
 import unittest
 import analyze_numbers
 import clicky_quad as click_quad
+import funcynum
+
 
 class AnalyzeNumbers(unittest.TestCase):
 
     def test_odd_true(self):
         self.assertTrue(analyze_numbers.is_odd(69))
 
+        
     def test_odd_false(self):
         self.assertFalse(analyze_numbers.is_odd(66))
 
+        
     def test_prime_true(self):
         self.assertTrue(analyze_numbers.is_prime(7))
 
+        
     def test_prime_false(self):
         self.assertFalse(analyze_numbers.is_prime(8))
 
+        
     def test_abundant_true(self):
         self.assertTrue(analyze_numbers.is_abundant(12))
 
+        
     def test_abundant_false(self):
         self.assertFalse(analyze_numbers.is_abundant(13))
 
+        
 class ClickQuadrant(unittest.TestCase):
 
     def test_upper_right(self):
         self.assertEqual("red", click_quad.get_quadrant_color(12, 12))
 
+        
     def test_lower_right(self):
         self.assertEqual("green", click_quad.get_quadrant_color(12, -12))
 
+        
     def test_upper_left(self):
         self.assertEqual("blue", click_quad.get_quadrant_color(-12, 12))
 
+        
     def test_lower_left(self):
         self.assertEqual("yellow", click_quad.get_quadrant_color(-12, -12))
-        
+
+
+class Funcynum(unittest.TestCase):
+
+    def test_horizontal_line(self):
+        self.assertEqual("  xx", funcynum.horizontal_line("x", 4, 2))
+
+
+    def test_vertical_lines(self):
+        self.assertEqual(" xx\n xx", funcynum.vertical_lines("x", 2, 1, 2, 0))
+
+
 if __name__ == '__main__':
     unittest.main()
